867f3dddb939f44960d0923119222d7faa11a8dc
Paco Duhard-Jourdan <pduhardjourdan@wanadev.fr>
2024-01-15 14:57:03 +0100

    Update yarn

--- contracts/ORIToken.sol
+++ contracts/ORIToken.sol
@@ -1,17 +1,31 @@
 // contracts/ORIToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.22;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-/**
- * @dev Implementation of the ORIToken contract.
- * Based on the OpenZeppelin implementation of ERC20, ERC20Burnable and ERC20Permit.
- */
-contract ORIToken is ERC20, ERC20Burnable, ERC20Permit {
-    constructor(uint256 initialSupply) ERC20("Orivium", "ORI") ERC20Permit("Orivium") {
+/***********************************************************************************************
+ *               ...                                                                           *
+ *         *@@@@@@@@@@@-                    .                                                  *
+ *      .@@@@@@@@@@@@@@@@#                =@@@*             :@@@@                              *
+ *     *@@@@@@*.   :*@@@@@@               .@@@.              @@@:                              *
+ *    :@@@@@:         =@@@@@                                                                   *
+ *    @@@@@:           .@@@@* %@@#@@@@@@= .@@@: @@@     #@@= @@@: @@@     :@@@ @@@%     +%@+   *
+ *   .@@@@#             @@@@@ @@@@@@@@@@@@.@@@+ @@@+   -@@@ .@@@= @@@     -@@@ @@@@*   *@@@#   *
+ *   .@@@@=             @@@@@.@@@@     @@@+@@@+ =@@@   @@@= .@@@=.@@@.    -@@@.@@@@@. =@@@@*   *
+ *   .@@@@#             @@@@@.@@@=.   :@@@+@@@=  @@@- =@@@  .@@@=.@@@.    -@@@.@@@@@@.@@@@@-   *
+ *    @@@@@.           .@@@@@.@@@=@@@@@@@*.@@@-  #@@%.@@@:  .@@@: @@@:    =@@@.@@@@@@@@@@@@-   *
+ *    .@@@@@.         :@@@@@- @@@::@@@@*  .@@@-  :@@@#@@%   .@@@. #@@@.  :@@@@.@@@*%@@@@@@@-   *
+ *     +@@@@@@*:  .-#@@@@@@-  @@@.  @@@*  .@@@:   @@@@@@.    @@@. .@@@@@@@@@@: @@@* @@# @@@.   *
+ *      .@@@@@@@@@@@@@@@@@    -==    ===   ===    :@@@@+     ===    :@@@@@@:   -==.  -  ###    *
+ *        .%@@@@@@@@@@@+                                                                       *
+ *             -+++:                                                                           *
+ *                                                                                             *
+ **********************************************************************************************/
+
+contract ORIToken is ERC20, ERC20Burnable {
+    constructor(uint256 initialSupply) ERC20("Orivium", "ORI") {
         _mint(_msgSender(), initialSupply);
     }
 }
